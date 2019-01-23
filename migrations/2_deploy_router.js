f73f8cc723822eb2e90d5ece1e4d16ead57959b1
Amir <amiromayer@gmail.com>
2019-01-23 10:04:38 +0500

    Added Pixel and Color deployed export

--- migrations/2_deploy_router.js
+++ migrations/2_deploy_router.js
@@ -1,10 +1,18 @@
 var ERC1538Delegate = artifacts.require("ERC1538Delegate");
 var Router = artifacts.require("Router");
+const PIXEL_CONTRACT_ADDRESS = process.env.PIXEL_CONTRACT_ADDRESS;
+const COLOR_CONTRACT_ADDRESS = process.env.COLOR_CONTRACT_ADDRESS;
 
 module.exports = async function(deployer) {
   deployer.deploy(ERC1538Delegate).then(function() {
-    return deployer.deploy(Router, ERC1538Delegate.address, {
-      value: web3.toWei(1)
-    });
+    return deployer.deploy(
+      Router,
+      ERC1538Delegate.address,
+      PIXEL_CONTRACT_ADDRESS,
+      COLOR_CONTRACT_ADDRESS,
+      {
+        value: web3.toWei(1)
+      }
+    );
   });
 };
